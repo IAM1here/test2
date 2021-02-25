3d19ac065610c49921a607aaf8333b211d5c4a7e
Iamthere <35186937+IAM1here@users.noreply.github.com>
2021-02-25 14:09:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Subdomain  Takeover POC by hackerone.com/rootmatters"
+    return "Subdomain  Takeover POC by iam1here"
+    return "The Subdomain is owned by microsoft and also this is just for demonstration"
